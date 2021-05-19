9e7c5e833e71feafcb5e97d7e092bc68687c3a68
Elena <elena.bogacheva97@gmail.com>
2021-05-19 22:43:51 +0300

    testNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,37 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+    @Test
+    public void shouldReturn1IfAmount999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void shouldNothingReturn1IfAmount1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999IfAmount1001() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
 }
+
+
